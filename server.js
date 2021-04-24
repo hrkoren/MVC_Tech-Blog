0401095f59a2556c8d880f9f0c553d781ea1c680
HRush <hrkoren@gmail.com>
2021-04-24 13:58:34 -0400

    started file structure and ran npm installs

---
+++ server.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const express = require('express');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
