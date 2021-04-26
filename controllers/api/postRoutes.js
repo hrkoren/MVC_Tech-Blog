a5d3072fbb1b28ac8aae4b419226750945f365df
HRush <hrkoren@gmail.com>
2021-04-25 21:31:08 -0400

    started route coding

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../../models');
+
+//get all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'coneent', 'username', 'date_created'],
+        // include: [
+        //     {
+        //         model: 
+        //     }
+        // ]
+    })
+        .then(data => {
+            if (!data) {
+                res.status(404).json({ message: 'No blog posts yet' });
+                return;
+            }
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.json(err);
+        });
+});
+
+//creat new post
+router.post('/', (req, res) => {
+    Post.create(req.body)
+        .then((post) => {
+            if (req.body.post)
+    })
+})
+
