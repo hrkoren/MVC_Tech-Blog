d4223aca49b7b47f3cecc3b891dfc2582799601b
HRush <hrkoren@gmail.com>
2021-04-27 18:22:49 -0400

    worked on homeroutes code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,26 @@
 const router = require('express').Router();
 const { Post, Comment } = require('../models');
+const sequelize = require('../config/connection');
 
 //get all posts for homepage
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({
+            attributes: [
+                'id',
+                'title',
+                'date_posted',
+                'content'
+            ],
             include: [
                 {
                     model: Comment,
-                    attributes: ['content', 'user_id', 'date_created'],
+                    attributes: ['content', 'user_id', 'date_posted'],
                 },
+                {
+                    model: User,
+                    attributes: ['username', 'email', 'password']
+                }
             ],
         });
         const posts = postData.map((post) =>
