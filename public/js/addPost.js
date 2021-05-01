a9014f2bc83a039be75e98a837e194b22c519b7b
HRush <hrkoren@gmail.com>
2021-05-01 14:26:56 -0400

    worked on issues with handlebars and js code not connecting

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -1,11 +1,11 @@
 //Post form handler
 async function newFormHandler(event) {
     event.preventDefault();
-
+    console.log('#newPost');
     const title = document.querySelector('input[name="title"]').value;
     const content = document.querySelector('content[name="content"]').value;
 
-    const response = await fetch(`/api/posts`, {
+    const response = await fetch('/api/posts', {
         method: 'POST',
         body: JSON.stringify({
             title,
@@ -16,10 +16,10 @@ async function newFormHandler(event) {
         }
     });
     if (response.ok) {
-        document.location.replace('/dashboard');
+        window.location.replace('/posts');
     } else {
         console.log(response.statusText);
     }
 }
 
-document.querySelector('.postForm').addEventListener('submit', newFormHandler);
+document.getElementById('newPost').addEventListener('submit', newFormHandler);
